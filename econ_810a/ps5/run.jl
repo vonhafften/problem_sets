7c701f19c9280648ff8a9c186ba4278e60d4e23d
vonhafften <alexanderhvonhafften@gmail.com>
2022-02-28 12:57:09 -0600

    presentation for fmi added

--- econ_810a/ps5/run.jl
+++ econ_810a/ps5/run.jl
@@ -17,59 +17,87 @@ Solve!(R)
 
 # investment at t=8
 
-plot(R.pf_i_8[:, :, 1], color=:lightgray, label = false, title = "Investment at t = 8 for Child with Lowest HC")
-plot!(R.pf_i_8[:, P.N_h, 1], label = "Parent with Highest HC", color = :red, legend = :bottomright)
-plot!(R.pf_i_8[:, 1, 1], label = "Parent with Lowest HC", color = :blue)
+plot(P.grid_b, R.pf_i_8[:, :, 1], color=:lightgray, label = false, title = "Investment at t = 8 for Child with Lowest HC")
+plot!(P.grid_b, R.pf_i_8[:, P.N_h, 1], label = "Parent with Highest HC", color = :red, legend = :topleft)
+plot!(P.grid_b, R.pf_i_8[:, 1, 1], label = "Parent with Lowest HC", color = :blue)
+xlabel!("Assets")
+ylabel!("Investment")
 
 savefig("i_c_l.png")
 
-plot(R.pf_i_8[:, :, P.N_h], color=:lightgray, label = false, title = "Investment at t = 8 for Child with Highest HC")
-plot!(R.pf_i_8[:, P.N_h, P.N_h], label = "Parent with Highest HC", color = :red, legend = :bottomright)
-plot!(R.pf_i_8[:, 1, P.N_h], label = "Parent with Lowest HC", color = :blue)
+plot(P.grid_b, R.pf_i_8[:, :, P.N_h], color=:lightgray, label = false, title = "Investment at t = 8 for Child with Highest HC")
+plot!(P.grid_b, R.pf_i_8[:, P.N_h, P.N_h], label = "Parent with Highest HC", color = :red, legend = :topleft)
+plot!(P.grid_b, R.pf_i_8[:, 1, P.N_h], label = "Parent with Lowest HC", color = :blue)
+xlabel!("Assets")
+ylabel!("Investment")
 
 savefig("i_c_h.png")
 
-plot(R.pf_i_8[:, 1, :], color=:lightgray, label = false, title = "Investment at t = 8 by Parent with Lowest HC")
-plot!(R.pf_i_8[:, 1, 1], label = "Child with Lowest HC", color = :red, legend = :bottomright)
-plot!(R.pf_i_8[:, 1, P.N_h], label = "Child with Highest HC", color = :blue, legend = :bottomright)
+plot(P.grid_b, R.pf_i_8[:, 1, :], color=:lightgray, label = false, title = "Investment at t = 8 by Parent with Lowest HC")
+plot!(P.grid_b, R.pf_i_8[:, 1, P.N_h], label = "Child with Highest HC", color = :red, legend = :topleft)
+plot!(P.grid_b, R.pf_i_8[:, 1, 1], label = "Child with Lowest HC", color = :blue)
+xlabel!("Assets")
+ylabel!("Investment")
 
 savefig("i_p_l.png")
 
-plot(R.pf_i_8[:, P.N_h, :], color=:lightgray, label = false, title = "Investment at t = 8 by Parent with Highest HC")
-plot!(R.pf_i_8[:, P.N_h, 1], label = "Child with Lowest HC", color = :red, legend = :bottomright)
-plot!(R.pf_i_8[:, P.N_h, P.N_h], label = "Child with Highest HC", color = :blue, legend = :bottomright)
+plot(P.grid_b, R.pf_i_8[:, P.N_h, :], color=:lightgray, label = false, title = "Investment at t = 8 by Parent with Highest HC")
+plot!(P.grid_b, R.pf_i_8[:, P.N_h, P.N_h], label = "Child with Highest HC", color = :red, legend = :topleft)
+plot!(P.grid_b, R.pf_i_8[:, P.N_h, 1], label = "Child with Lowest HC", color = :blue)
+xlabel!("Assets")
+ylabel!("Investment")
 
 savefig("i_p_h.png")
 
 # transfers plots
 
-plot(R.pf_τ_9[:, :, 1], color=:lightgray, label = false, title = "Transfer for Child with Lowest HC")
-plot!(R.pf_τ_9[:, P.N_h, 1], label = "Parent with Highest HC", color = :red, legend = :bottomright)
-plot!(R.pf_τ_9[:, 1, 1], label = "Parent with Lowest HC", color = :blue)
+plot(P.grid_b, R.pf_τ_9[:, :, 1], color=:lightgray, label = false, title = "Transfer for Child with Lowest HC")
+plot!(P.grid_b, R.pf_τ_9[:, P.N_h, 1], label = "Parent with Highest HC", color = :red, legend = :bottomright)
+plot!(P.grid_b, R.pf_τ_9[:, 1, 1], label = "Parent with Lowest HC", color = :blue)
+xlabel!("Assets")
+ylabel!("Transfer")
 
 savefig("tau_c_l.png")
 
-plot(R.pf_τ_9[:, :, P.N_h], color=:lightgray, label = false, title = "Transfer for Child with Highest HC")
-plot!(R.pf_τ_9[:, P.N_h, P.N_h], label = "Parent with Highest HC", color = :red, legend = :bottomright)
-plot!(R.pf_τ_9[:, 1, P.N_h], label = "Parent with Lowest HC", color = :blue)
+plot(P.grid_b, R.pf_τ_9[:, :, P.N_h], color=:lightgray, label = false, title = "Transfer for Child with Highest HC")
+plot!(P.grid_b, R.pf_τ_9[:, P.N_h, P.N_h], label = "Parent with Highest HC", color = :red, legend = :bottomright)
+plot!(P.grid_b, R.pf_τ_9[:, 1, P.N_h], label = "Parent with Lowest HC", color = :blue)
+xlabel!("Assets")
+ylabel!("Transfer")
 
 savefig("tau_c_h.png")
 
-plot(R.pf_τ_9[:, 1, :], color=:lightgray, label = false, title = "Transfer from Parent with Lowest HC")
-plot!(R.pf_τ_9[:, 1, 1], label = "Child with Lowest HC", color = :red, legend = :bottomright)
-plot!(R.pf_τ_9[:, 1, P.N_h], label = "Child with Highest HC", color = :blue, legend = :bottomright)
+plot(P.grid_b, R.pf_τ_9[:, 1, :], color=:lightgray, label = false, title = "Transfer from Parent with Lowest HC")
+plot!(P.grid_b, R.pf_τ_9[:, 1, P.N_h], label = "Child with Highest HC", color = :red, legend = :bottomright)
+plot!(P.grid_b, R.pf_τ_9[:, 1, 1], label = "Child with Lowest HC", color = :blue, legend = :bottomright)
+xlabel!("Assets")
+ylabel!("Transfer")
 
 savefig("tau_p_l.png")
 
-plot(R.pf_τ_9[:, P.N_h, :], color=:lightgray, label = false, title = "Transfer from Parent with Highest HC")
-plot!(R.pf_τ_9[:, P.N_h, 1], label = "Child with Lowest HC", color = :red, legend = :bottomright)
-plot!(R.pf_τ_9[:, P.N_h, P.N_h], label = "Child with Highest HC", color = :blue, legend = :bottomright)
+plot(P.grid_b, R.pf_τ_9[:, P.N_h, :], color=:lightgray, label = false, title = "Transfer from Parent with Highest HC")
+plot!(P.grid_b, R.pf_τ_9[:, P.N_h, P.N_h], label = "Child with Highest HC", color = :red, legend = :bottomright)
+plot!(P.grid_b, R.pf_τ_9[:, P.N_h, 1], label = "Child with Lowest HC", color = :blue, legend = :bottomright)
+xlabel!("Assets")
+ylabel!("Transfer")
 
 savefig("tau_p_h.png")
 
 
 S = Initialize_simulation(10000)
 Simulate!(R, S)
+
+plot(4:12, mean(S.h, dims = 1)', title = "Mean Human Capital", legend = false)
+xlabel!("Model Age")
+ylabel!("Human Capital")
+
+savefig("mean_hc.png")
+
+plot(4:12, var(S.h, dims = 1)', title = "Cross Sectional Variance of Human Capital", legend = false)
+xlabel!("Model Age")
+ylabel!("Human Capital^2")
+
+savefig("var_hc.png")
 
 plot(4:12, mean(S.b, dims = 1)', title = "Mean Assets", legend = false)
 xlabel!("Model Age")
